72bdbd4bc791dd12dbd29452de88673437730b61
Rômulo A. Ceccon <romuloceccon@gmail.com>
2016-04-24 21:55:06 -0300

    Melhora legibilidade do dispatcher

--- lib/processor.rb
+++ lib/processor.rb
@@ -10,39 +10,58 @@ while true do
 end
 EOS
 
+  EVENTS_MAIN_QUEUE = "events:queue".freeze
+  EVENTS_TEMP_QUEUE = "events:dispatching".freeze
+
+  OPERATORS_QUEUE = "operators:queue".freeze
+  OPERATORS_KNOWN_SET = "operators:known".freeze
+
   class Dispatcher
     def initialize(redis)
       @redis = redis
     end
 
-    def dispatch_all
-      while @redis.exists("events:queue") do
-        @redis.rename("events:queue", "events:dispatching")
+    def dispatch_all(&block)
+      while @redis.exists(EVENTS_MAIN_QUEUE) do
+        @redis.rename(EVENTS_MAIN_QUEUE, EVENTS_TEMP_QUEUE)
+        list = prepare_events(EVENTS_TEMP_QUEUE, &block)
 
-        cnt = @redis.llen("events:dispatching")
-        arr = (1..cnt).map do |i|
-          json = @redis.lindex("events:dispatching", -i)
+        @redis.multi do
+          enqueue_events(list)
+          @redis.del(EVENTS_TEMP_QUEUE)
+        end
+      end
+    end
+
+    private
+      def prepare_events(queue_name)
+        cnt = @redis.llen(queue_name)
+
+        (1..cnt).map do |i|
+          json = @redis.lindex(queue_name, -i)
           data = JSON.parse(json)
           operator_id, installation_id = yield(data)
           [operator_id, installation_id, data]
         end
+      end
 
-        @redis.multi do
-          seen = []
-          arr.each do |(operator_id, installation_id, data)|
-            op_str = operator_id.to_s
+      def enqueue_events(events)
+        seen = []
+        events.each do |(operator_id, installation_id, data)|
+          op_str = operator_id.to_s
 
-            @redis.sadd("operators:known", op_str)
-            @redis.lpush("operators:queue", op_str) unless seen.include?(op_str)
-            seen << op_str
+          @redis.sadd(OPERATORS_KNOWN_SET, op_str)
+          @redis.lpush(OPERATORS_QUEUE, op_str) unless seen.include?(op_str)
+          seen << op_str
 
-            @redis.lpush("operator:#{op_str}:events",
-              { 'installation_id' => installation_id, 'data' => data }.to_json)
-          end
-          @redis.del("events:dispatching")
+          @redis.lpush(operator_queue(op_str),
+            { 'installation_id' => installation_id, 'data' => data }.to_json)
         end
       end
-    end
+
+      def operator_queue(id)
+        "operator:#{id}:events"
+      end
   end
 
   def initialize(redis)
@@ -52,7 +71,7 @@ EOS
   def dispatcher
     return @dispatcher if @dispatcher
 
-    @redis.eval(LUA_JOIN_LISTS, ["events:dispatching", "events:queue"])
+    @redis.eval(LUA_JOIN_LISTS, [EVENTS_TEMP_QUEUE, EVENTS_MAIN_QUEUE])
     @dispatcher = Dispatcher.new(@redis)
   end
 
